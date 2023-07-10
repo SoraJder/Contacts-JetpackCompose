de20e1122ed6ab77ad56b5c22185e1f3001f24f6
Alina <alinateca2001@gmail.com>
2023-07-10 21:21:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/common/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.common
+
+object Constants {
+
+    const val BASE_URL = "https://gorest.co.in/public/v2/"
+    const val PARAM_USER_ID = "userID"
+}
---
+++ app/src/main/java/com/example/common/Resource.kt
@@ -0,0 +1,12 @@
+package com.example.common
+
+sealed class Resource<out T> {
+    object Loading : Resource<Nothing>()
+    data class Success<out T>(
+        val data: T
+    ) : Resource<T>()
+
+    data class Failure(
+        val e: Exception
+    ) : Resource<Nothing>()
+}
